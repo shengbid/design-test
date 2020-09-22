7e62fe8969a6ab9a454d1a782c9d0b110f73a893
laism <laism@chinacsci.com>
2020-09-22 14:52:50 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import Design from './pages/design/index.tsx'
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a> */}
+        <Design></Design>
       </header>
     </div>
   );
